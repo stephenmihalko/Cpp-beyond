9b09047cb68d510b43528574cbb2d475c1103c05
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-05 18:02:12 -0500

    Update workspace
    Remove first project
    Create new project
    Write amazingly simple code

---
+++ BeginToBeyond/FirstProgram/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int favNum;
+    std::cout << "Please enter your favorite number: ";
+    std::cin >> favNum;
+    std::cout << "My favorite number is " << favNum << " too!" << std::endl;
+    return 0;
+}
