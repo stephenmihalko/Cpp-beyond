ad4d67e5fd91d9f38385c9c99fac452d24e21bbc
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-15 14:45:42 -0400

    Implement substitution cipher

--- BeginToBeyond/Arrays/main.cpp
+++ BeginToBeyond/Arrays/main.cpp
@@ -7,5 +7,13 @@ int main()
     string alphabet {"abcdefghijklmnopqrstuvwxyz0123456789 ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
     string code {"m8fk3Dlcj6CEn1rb2eoFqAgdLs9BNTGZ0SYpRUX MWQiKOVwy5HPt4aJzxv7Ihu"};
     
+    string input;
     
+    cout << "Please enter a word to encode: ";
+    getline(cin, input);
+    
+    for (size_t i = 0; i < input.length(); ++i)
+        input[i] = code[alphabet.find(input[i])];
+        
+    cout << "Your encoded word is " << input << endl;
 }
