e922aff725be6a155a99487c0c602b48d1abda78
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-15 14:38:06 -0400

    Rename project to Substitution_Cipher

--- BeginToBeyond/Arrays/main.cpp
+++ BeginToBeyond/Arrays/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
 int main()
 {
-    cout << 46 % 5 << endl;
+    string alphabet {"abcdefghijklmnopqrstuvwxyz0123456789 ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    string code {"m8fk3Dlcj6CEn1rb2eoFqAgdLs9BNTGZ0SYpRUX MWQiKOVwy5HPt4aJzxv7Ihu"};
+    
+    
 }
